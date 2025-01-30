e65f38812a106daee8d83b33f867e774229683ea
George Brunt <dexter@brunts.org>
2025-01-30 14:27:55 -0700

    Simplified Queen, Bishop, and Rook calculators greatly

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -7,45 +7,15 @@ public class BishopMovesCalculator extends PieceMovesCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
-        int currentRow = position.getRow();
-        int currentCol = position.getColumn();
         ChessPiece bishop = board.getPiece(position);
 
         if (bishop == null) {
             return moves;
         }
+        this.teamColor = bishop.getTeamColor();
 
-        int[][] directions = {
-                {-1, -1}, {-1, 1},
-                {1, -1}, {1, 1}
-        };
 
-        for (int[] direction : directions) {
-            int newRow = currentRow;
-            int newCol = currentCol;
-
-            while (true) {
-                newRow += direction[0];
-                newCol += direction[1];
-
-                if (!isValidPosition(newRow, newCol)) {
-                    break;
-                }
-
-                ChessPosition newPosition = new ChessPosition(newRow, newCol);
-                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
-
-                if (pieceAtNewPosition == null) {
-                    moves.add(new ChessMove(position, newPosition, null));
-                } else {
-                    if (pieceAtNewPosition.getTeamColor() != bishop.getTeamColor()) {
-                        moves.add(new ChessMove(position, newPosition, null));
-                    }
-                    break; // Stop after capturing or encountering own piece
-                }
-            }
-        }
-
+        moves.addAll(calculateDiagonalMoves(board, position));
         return moves;
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -7,46 +7,14 @@ public class RookMovesCalculator extends PieceMovesCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
-        int currentRow = position.getRow();
-        int currentCol = position.getColumn();
         ChessPiece rook = board.getPiece(position);
 
         if (rook == null) {
             return moves;
         }
-
-        int[][] directions = {
-                        {-1, 0},
-                {0, -1},        {0, 1},
-                        {1, 0}
-        };
-
-        for (int[] direction : directions) {
-            int newRow = currentRow;
-            int newCol = currentCol;
-
-            while (true) {
-                newRow += direction[0];
-                newCol += direction[1];
-
-                if (!isValidPosition(newRow, newCol)) {
-                    break;
-                }
-
-                ChessPosition newPosition = new ChessPosition(newRow, newCol);
-                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
-
-                if (pieceAtNewPosition == null) {
-                    moves.add(new ChessMove(position, newPosition, null));
-                } else {
-                    if (pieceAtNewPosition.getTeamColor() != rook.getTeamColor()) {
-                        moves.add(new ChessMove(position, newPosition, null));
-                    }
-                    break; // Stop after capturing or encountering own piece
-                }
-            }
-        }
-
+        this.teamColor = rook.getTeamColor();
+        moves.addAll(calculateStraightMoves(board, position));
         return moves;
     }
 }
+
